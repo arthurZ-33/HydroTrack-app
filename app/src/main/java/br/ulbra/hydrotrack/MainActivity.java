ec6598d8fc7a6142eb8cfe490387dc7c10e39365
arthurZ-33 <arthur.rodrigue64@gmail.com>
2025-10-06 20:04:35 -0300

    v1.0.2

--- app/src/main/java/br/ulbra/hydrotrack/MainActivity.java
+++ app/src/main/java/br/ulbra/hydrotrack/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     RadioButton btnSedentario, btnModerado, btnIntenso;
     Button btnCalcularMeta;
-    EditText txtPeso, txtIdade;
-    TextView txtConsumo,txtvMeta,txtvRestante ;
-    RadioGroup btnGrupo;
+    EditText txtPeso, txtIdade, txtIngestao;
+    TextView txtvRestante,  txtMeta, txtSugestao;
+    RadioGroup rbtAtividade;
 
 
 
@@ -32,32 +32,46 @@ public class MainActivity extends AppCompatActivity {
         btnSedentario = findViewById(R.id.btnSedentario);
         btnModerado = findViewById(R.id.btnModerado);
         btnIntenso = findViewById(R.id.btnIntenso);
-        btnGrupo = findViewById(R.id.rbtAtividade);
-        txtConsumo = findViewById(R.id.txtConsumo);
+        rbtAtividade = findViewById(R.id.rbtAtividade);
         txtvRestante = findViewById(R.id.txtvRestante);
-        txtvMeta = findViewById(R.id.txtvMeta);
+        txtIngestao = findViewById(R.id.txtIngestao);
+        txtMeta = findViewById(R.id.txtMeta);
+        txtSugestao = findViewById(R.id.txtSugestao);
 
 
 
         btnCalcularMeta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double consumo_base, peso, consumo_total, restante, extra =0;
-
+                double consumo_base, peso, consumo_total, restante, ingestao, extra =0;
+                ingestao = Double.parseDouble(txtIngestao.getText().toString());
                 peso = Double.parseDouble(txtPeso.getText().toString());
+
                 consumo_base = peso * 35;
                 consumo_total = consumo_base + extra;
-                restante = consumo_total - consumo_base;
+                restante = consumo_total - ingestao;
 
-                int idSelecionado = btnGrupo.getCheckedRadioButtonId();
+                int idSelecionado = rbtAtividade.getCheckedRadioButtonId();
 
-                if(idSelecionado == btnSedentario){
+                if(idSelecionado == R.id.btnSedentario){
                     extra = 0;
                 } else if (idSelecionado == R.id.btnModerado) {
                     extra = 300;
                 } else if (idSelecionado == R.id.btnIntenso) {
                     extra = 600;
                 }
+
+                txtvRestante.setText("O restante será de: " + restante);
+                txtMeta.setText("A meta será: " + consumo_total);
+
+                if (idSelecionado == R.id.btnSedentario){
+                    txtSugestao.setText("Beba mais água.");
+                }else if (idSelecionado == R.id.btnModerado){
+                    txtSugestao.setText("Beba um pouco mais de água.");
+                }else if (idSelecionado == R.id.btnIntenso){
+                    txtSugestao.setText("Continue assim!");
+                }
+
             }
         });
 
